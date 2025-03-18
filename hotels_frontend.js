381d69b82864fe197e27ee6e965687c9d7a2e5dd
That-CuteVegan <magn1.mt@gmail.com>
2025-03-18 08:24:04 +0100

    Update hotels_frontend.js

--- hotels_frontend.js
+++ hotels_frontend.js
@@ -1,15 +1,16 @@
-<!--
- * Version: 1.0
- * Author: That_CuteVegan
- -->
+// Version: 1.0
+// Author: That_CuteVegan
 
 document.addEventListener("DOMContentLoaded", async function() {
     const container = document.getElementById("hotels-container");
     
     try {
+
+        // Waits on a response from database and posts the informations at "/wp-json/hotel-api/v1/hotels"
         const response = await fetch("/wp-json/hotel-api/v1/hotels");
         const hotels = await response.json();
         
+        // Foreach loop looking in to how many hotels we have and makes a crate element per hotel
         hotels.forEach(hotel => {
             const div = document.createElement("div");
             div.className = "hotel-box";
@@ -20,6 +21,8 @@ document.addEventListener("DOMContentLoaded", async function() {
             `;
             container.appendChild(div);
         });
+
+    // In case of an error, error is shown directly on the console
     } catch (error) {
         console.error("Error fetching hotels:", error);
     }
